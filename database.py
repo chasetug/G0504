d380ef277ca969064dae26e1889c7cde38b7fbff
chasetug <chasetug@gmail.com>
2021-11-11 17:14:06 -0600

    added edit time function

--- database.py
+++ database.py
@@ -17,14 +17,14 @@ def add_item(name, timestamp):
     with open('database.csv', 'r') as csvfile:
         csv_reader = csv.reader(csvfile)
         for row in csv_reader:
-            if row[0] != name:
-                lines.append(row)
+            lines.append(row)
 
     lines.append([name, timestamp])
 
     with open('database.csv', 'w') as csvfile:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerows(lines)
+
 
 def del_item(name):
     lines = list()
@@ -33,6 +33,20 @@ def del_item(name):
         for row in csv_reader:
             if row[0] != name:
                 lines.append(row)
+
+    with open('database.csv', 'w') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerows(lines)
+
+
+def edit_time(name, new_time):
+    lines = list()
+    with open('database.csv', 'r') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for row in csv_reader:
+            if row[0] == name:
+                row[1] = new_time
+            lines.append(row)
 
     with open('database.csv', 'w') as csvfile:
         csv_writer = csv.writer(csvfile)
