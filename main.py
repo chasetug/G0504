0c2d04b0b9919a46ead64d66db194dca0b407f47
chasetug <chasetug@gmail.com>
2021-11-18 13:15:24 -0600

    merge gui

--- main.py
+++ main.py
@@ -1,66 +1,93 @@
 import database
+import tkinter as tk
+from tkinter import *
 
 if __name__ == "__main__":
+    task_list = []
 
-    loop = 1
 
-    # Infinite loop to continuously ask for tasks/edit tasks
-    while loop == 1:
-        print(database.read_database())
+    def addTask(*args):
+        global task_list
+        task = task_entry.get()
+        if task:
+            with open("task.txt", 'a') as taskfile:
+                taskfile.write("\n" + task)
+            task_list.append(task)
+            listbox.insert(tk.END, task)
 
-        # Asks the user to select an action
-        print('1) Add task  |  2) edit task')
-        taskCHOICE = int(input())
 
-        # if statement for if the user chooses to add a task
-        if taskCHOICE == 1:
+    def deleteTask():
+        global task_list
+        task = listbox.get(tk.ANCHOR)
+        if task in task_list:
+            task_list.remove(task)
+            with open("task.txt", 'w') as taskfile:
+                for task in task_list:
+                    taskfile.write(task + "\n")
+            listbox.delete(tk.ANCHOR)
 
-            # asks for the task name and the time due
-            taskNAME = input('Enter a task: ')
-            dueDATE = database.input_time()
 
-            addTASK = [taskNAME, int(dueDATE.timestamp())]
+    def CheckTask():
+        global task_list
+        task = listbox.get(tk.ANCHOR)
+        for item in listbox.curselection():
+            sel = item
+        if task in task_list:
+            listbox.itemconfig(sel, {'bg': 'light green'})
 
-            # adds the task name (addTASK[0]) and unix timestamp value (addTASK[1]) to the database
-            database.add_item(addTASK[0], addTASK[1])
 
-        # if statement for if the user chooses to edit a task
-        elif taskCHOICE == 2:
+    def UncheckTask():
+        global task_list
+        task = listbox.get(tk.ANCHOR)
+        for item in listbox.curselection():
+            sel = item
+        if task in task_list:
+            listbox.itemconfig(sel, {'bg': 'white'})
 
-            print('1) Change task name   |   2) Change task due date   |   3) Delete Task   | 4) Return Home')
-            editTASK_choice = int(input())
 
-            changeTASK = ''
-            editTASK_loop = 0
+    def DelAll():
+        listbox.delete(0, END)
 
-            while editTASK_loop == 0:
 
-                # if statement for if the user chooses to change task name
-                if editTASK_choice == 1:
-                    changeTASK = input('Enter the name of a task to change it\'s name: ')
-                    newName = input('Enter the new name of the selected task: ')
-                    editTASK_loop = database.edit_name(changeTASK, newName)
+    def openTaskFile():
+        global task_list
+        with open("task.txt", 'r') as taskfile:
+            tasks = taskfile.readlines()
+        print(tasks)
+        for task in tasks:
+            if task != '\n':
+                task_list.append(task)
+                listbox.insert(tk.END, task)
 
-                    if editTASK_loop == 0:
-                        print('Name not found! Please try entering another name!')
 
-                # if statement for if the user chooses to change task due date
-                elif editTASK_choice == 2:
-                    changeTASK = input('Enter the name of a task to change it\'s due date: ')
-                    changeTIME = database.input_time()
-                    editTASK_loop = database.edit_time(changeTASK, changeTIME)
-                    if editTASK_loop == 0:
-                        print('Name not found! Please try entering another name!')
-
-                # if statement for if the user chooses to delete a task
-                elif editTASK_choice == 3:
-                    # deletes the task that matches the user's input
-                    changeTASK = input('Enter the name of a task to delete: ')
-                    editTASK_loop = database.del_item(changeTASK)
-                    if editTASK_loop == 0:
-                        print('Name not found! Please try entering another name!')
-
-                # if statement for if the user chooses to go back to home menu
-                elif editTASK_choice == 4:
-                    continue
-
+    root = tk.Tk()
+    root.title("TO DO LIST")
+    root.geometry("400x360")
+    root.config(bg="#AFAFD7")
+    root.resizable(0, 0)
+    frame = tk.Frame(root, bd=3, width=300, height=350)
+    frame.pack(pady=10)
+    listbox = tk.Listbox(frame, font=('arial', 12), width=40, height=12)
+    listbox.pack(side=tk.LEFT, fill=tk.BOTH, padx=2)
+    openTaskFile()
+    scrollbar = tk.Scrollbar(frame)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.BOTH)
+    listbox.config(yscrollcommand=scrollbar.set)
+    scrollbar.config(command=listbox.yview)
+    #listbox / tasklist
+task_entry = tk.Entry(root, width=25, font=('times', 14))
+task_entry.pack(pady=10)
+task_entry.bind("<Return>", addTask)
+btn_frame = tk.Frame(root, width=280, height=5)
+btn_frame.pack(pady=0)
+add_task_btn = tk.Button(btn_frame, text=" Add Task ", bg='gold', command=addTask)
+add_task_btn.grid(row=0, column=0)
+del_task_btn = tk.Button(btn_frame, text=" Delete Task ", bg='orange', command=deleteTask)
+del_task_btn.grid(row=0, column=3)
+check_task_btn = tk.Button(btn_frame, text=" Check Task ", bg='light green', command=CheckTask)
+check_task_btn.grid(row=0, column=1)
+uncheck_task_btn = tk.Button(btn_frame, text=" UnCheck Task ", bg='white', command=UncheckTask)
+uncheck_task_btn.grid(row=0, column=2)
+del_all_btn = tk.Button(btn_frame, text="     CLEAR     ", bg='red', command=DelAll)
+del_all_btn.grid(row=0, column=4)
+root.mainloop()
